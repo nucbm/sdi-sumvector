6eb52f3fdc190c7c6ab76498b648a89d6798b4e0
nucbm <stud.eeng@gmail.com>
2021-11-02 13:42:59 +0200

    Add files via upload

---
+++ TestClient.java
@@ -0,0 +1,26 @@
+import java.rmi.*;
+
+public class TestClient {
+   public static void main (String args []) {
+    String n1 = "123";
+    String n2 = "456";
+    
+    int[] v = {1,2,3,4,5};
+    
+    
+    try {
+     RmiInterface rmiinter= (RmiInterface)Naming.lookup("rmi://cti.ubm.ro/AdunaNumere");
+     System.out.println ("\n The first no is " + n1);
+     System.out.println ("\n The second no is "+ n2);
+     double d1, d2;
+     d1=Double.valueOf(n1).doubleValue();
+     d2=Double.valueOf(n2).doubleValue();
+     System.out.println ("\nSuma celor 2 numere: " + rmiinter.add (d1, d2));
+     System.out.println ("\nSuma elementelor vectorului: " + rmiinter.ADD(v));  
+    }
+    catch (Exception e) {
+        System.out.println ("An error occurred.");  
+    }
+   }  
+}
+
